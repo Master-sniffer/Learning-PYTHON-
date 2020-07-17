789e2f7b5d5fd0875979deb939a2f57d6e342265
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-17 21:55:05 +0300

    Update imports.py

--- Book №1/imports.py
+++ Book №1/imports.py
@@ -1,9 +1,20 @@
 #Here will be published imports from the Book 1. Number of the line where i used this import, will be provided
 
-##Line 1256
+##Line 1255
 def make_pizza(size, *toppings):
     print (f"The size of that baby is {size} and toppings are: ")
     for i in toppings:
         print (i)
 
-...
+
+        
+##Line 1289
+def print_models(unprinted,completed):
+    while unprinted:
+        current=unprinted.pop()
+        print (f"{current} is in progress")
+        completed.append(current)
+
+def show_models(completed):
+    for i in completed:
+        print (i)
