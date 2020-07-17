302887f8797252329855002b9a88546b7e813a53
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-17 21:39:55 +0300

    Update imports.py

--- Book №1/imports.py
+++ Book №1/imports.py
@@ -1,1 +1,9 @@
 #Here will be published imports from the Book 1. Number of the line where i used this import, will be provided
+
+##Line 1256
+def make_pizza(size, *toppings):
+    print (f"The size of that baby is {size} and toppings are: ")
+    for i in toppings:
+        print (i)
+
+...
