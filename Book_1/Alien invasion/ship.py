1439b1b483d262109e308aa0b2dae1476161c4e4
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-26 02:17:51 +0300

    Update ship.py

--- Book_1/Alien invasion/ship.py
+++ Book_1/Alien invasion/ship.py
@@ -6,7 +6,7 @@ class Ship():
         self.settings=ai_game.settings
         self.screen_rect=ai_game.screen.get_rect()
 
-        self.image=pygame.image.load('images/rocket-2058254_960_720.bmp')
+        self.image=pygame.image.load('images/pngaaa.com-795642.bmp')
         self.rect=self.image.get_rect()
 
         self.rect.midbottom=self.screen_rect.midbottom
