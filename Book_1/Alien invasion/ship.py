981f52cced09069c9f26c06ddeb57b6862589a00
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-25 03:02:48 +0300

    Update ship.py

--- Book_1/Alien invasion/ship.py
+++ Book_1/Alien invasion/ship.py
@@ -3,12 +3,35 @@ import pygame
 class Ship():
     def __init__(self,ai_game):
         self.screen=ai_game.screen
+        self.settings=ai_game.settings
         self.screen_rect=ai_game.screen.get_rect()
 
-        self.image=pygame.image.load('')
+        self.image=pygame.image.load('images/rocket-2058254_960_720.bmp')
         self.rect=self.image.get_rect()
 
         self.rect.midbottom=self.screen_rect.midbottom
+
+        self.x=float(self.rect.x)
+        self.y=float(self.rect.y)
+
+        #moving is down here
+        self.moving_right=False
+        self.moving_left=False
+        self.moving_up=False
+        self.moving_down=False
+    
+    def update(self):
+        if self.moving_right and self.rect.right<=self.screen_rect.right:
+            self.x+=self.settings.ship_speed
+        elif self.moving_left and self.rect.left>=0:
+            self.x-=self.settings.ship_speed
+        if self.moving_up and self.rect.top>0:
+            self.y-=self.settings.ship_speed
+        elif self.moving_down and self.rect.bottom<self.screen_rect.bottom:
+            self.y+=self.settings.ship_speed
+        
+        self.rect.x=self.x
+        self.rect.y=self.y
     
     def blitme(self):
         self.screen.blit(self.image, self.rect)
