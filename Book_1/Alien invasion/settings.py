abe3d38599641a19d504e6a18545a03d2eaf7155
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-25 03:02:34 +0300

    Update settings.py

--- Book_1/Alien invasion/settings.py
+++ Book_1/Alien invasion/settings.py
@@ -1,5 +1,6 @@
 class Settings():
     def __init__(self):
-        self.screen_width=1200
+        self.screen_width=1280
         self.screen_height=800
         self.bg_colour=(210,230,230)
+        self.ship_speed=1.5
