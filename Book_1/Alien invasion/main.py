313d02c020e6b3004da17423bbe4175a3e45569f
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-27 10:32:39 +0300

    Update main.py

--- Book_1/Alien invasion/main.py
+++ Book_1/Alien invasion/main.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullet()
+            self._update_aliens()
             self._update_screen()
 
     
@@ -43,8 +44,8 @@ class AlienInvasion:
         #self.charecter.blitme()  #MB I WILL DO IT  
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-        self.aliens.draw(self.screen)
-        pygame.display.flip()
+        self.aliens.draw(self.screen)#показывает пришельцев на self.screen (общий экран)
+        pygame.display.flip() #обновление экрана 
 
 
     def _check_events(self): #общая проверка ивентов
@@ -83,12 +84,12 @@ class AlienInvasion:
         elif event.key==pygame.K_DOWN:
             self.ship.moving_down=False
     
-    def _fire_bullet(self):
+    def _fire_bullet(self): #отвечает за запуск пули
         if len(self.bullets) < self.settings.bullet_allowed:
             new_bullet=Bullet(self)
             self.bullets.add(new_bullet)
     
-    def _update_bullet(self):
+    def _update_bullet(self): #отвечает за устраниение пули, когда она достигнет определенной точки
         self.bullets.update()
         for bullet in self.bullets.copy():
             if bullet.rect.bottom<=0:
@@ -96,11 +97,40 @@ class AlienInvasion:
 
         #print (len(self.bullets))
 
-    def _create_fleet(self):
+    def _create_fleet(self): #Создание флота пришельцев 
         alien=Alien(self)
+        alien_width, alien_height=alien.rect.size #метод alien.rect.size возвращает ширину и длину пришельца
+        ship_height=self.ship.rect.height #высота корабля
+        available_space_x=self.settings.screen_width-(2*alien_width) #настройки экрана - ширина пришельца * 2 (чтобы было место)
+        available_space_y=self.settings.screen_height-(3* alien_height) - ship_height #получаем настройки, чтобы узнать кол-во рядов, мы можем сделать
+        number_rows=available_space_y//(2*alien_height) #считаем точное кол-во, чтобы у игрока был шанс нормально играть без проблем
+        number_aliends=available_space_x//(2*alien_width) #кол-во пришельцев в ряду
+        for row in range (number_rows-4): #допилить вариацию ряда
+            for number in range(number_aliends):
+                self._create_alien(number,row) #для каждого нового пришельца, мы вызываем метод создания пришельца (указываем кол-во в ряду и кол-во рядом)
+    
+    def _create_alien(self, alien_number,row):
+        alien=Alien(self)
+        alien_width, alien_height=alien.rect.size
+        alien.x=alien_width+2* alien_width * alien_number #определили расстояние между инепрешеленцами
+        alien.rect.x=alien.x
+        alien.rect.y=alien_height +2*alien_height*row
         self.aliens.add(alien)
 
-
+    def _update_aliens(self):
+        self.aliens.update()
+        self._check_fleet()
+    
+    def _check_fleet(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_direction()
+                break
+    
+    def _change_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *=-1
 
 if __name__=='__main__':
     ai=AlienInvasion()
