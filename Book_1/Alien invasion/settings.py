a303842bf83b79ec33d1aca37b9b5fa953ce3efd
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-27 10:34:38 +0300

    Update settings.py

--- Book_1/Alien invasion/settings.py
+++ Book_1/Alien invasion/settings.py
@@ -3,4 +3,14 @@ class Settings():
         self.screen_width=1280
         self.screen_height=800
         self.bg_colour=(210,230,230)
-        self.ship_speed=1.5
+        self.ship_speed=7.0
+        #параметр пришельца
+        self.alien_speed=0.5
+        self.fleet_drop_speed=10
+        self.fleet_direction=1
+        #ПАРАМЕТР СНАРЯДА
+        self.bullet_speed=8
+        self.bullet_width=5
+        self.bullet_height=15
+        self.bullet_color=(50,50,20)
+        self.bullet_allowed=5
