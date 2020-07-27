917625874a4de60dd4d48c7b597ed7c14c782466
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-27 10:32:57 +0300

    Update alien.py

--- Book_1/Alien invasion/alien.py
+++ Book_1/Alien invasion/alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen=ai_game.screen
+        self.settings=ai_game.settings
 
         self.image=pygame.image.load('images/pngaaa.com-476369.bmp')
         self.rect=self.image.get_rect()
@@ -14,4 +15,14 @@ class Alien(Sprite):
         self.rect.y=self.rect.height
 
         self.x=float(self.rect.x)
+    
+    def update(self):
+        #self.x+=self.settings.alien_speed
+        self.x+=(self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.x=self.x
+    
+    def check_edges(self):
+        screen_rect=self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <=0:
+            return True
         
