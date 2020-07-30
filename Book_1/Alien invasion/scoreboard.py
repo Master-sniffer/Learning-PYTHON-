618a7723f5c1e884c570b4fe3fcaa1f194ba3b6d
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-31 01:27:32 +0300

    Update scoreboard.py

--- Book_1/Alien invasion/scoreboard.py
+++ Book_1/Alien invasion/scoreboard.py
@@ -10,9 +10,12 @@ class Scoreboard():
         self.text_color=(30,30,30)
         self.font=pygame.font.SysFont(None, 48)
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
     
     def prep_score(self):
-        score_str=str(self.stats.score)
+        rounded_score=round(self.stats.score, -1)
+        score_str="{:,}".format(rounded_score)
         self.score_image=self.font.render(score_str, True, self.text_color, self.settings.bg_colour)
 
         self.score_rect=self.score_image.get_rect()
@@ -21,3 +24,26 @@ class Scoreboard():
     
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+    
+    def prep_high_score(self):
+        high_score=round(self.stats.high_score, -1)
+        high_score_str="{:,}".format(high_score)
+        self.high_score_image=self.font.render(high_score_str, True, self.text_color, self.settings.bg_colour)
+
+        self.high_score_rect=self.high_score_image.get_rect()
+        self.high_score_rect.centerx=self.screen_rect.centerx
+        self.high_score_rect.top=self.score_rect.top
+    
+    def check_high_score(self):
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score=self.stats.score
+            self.prep_high_score()
+    
+    def prep_level(self):
+        level_str=str(self.stats.level)
+        self.level_image=self.font.render(level_str, True, self.text_color, self.settings.bg_colour)
+        self.level_rect=self.level_image.get_rect()
+        self.level_rect.right=self.screen_rect.right
+        self.level_rect.top=self.score_rect.bottom + 10
