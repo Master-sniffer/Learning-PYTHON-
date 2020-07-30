5903cd9b33cd183bd32caf048a387ba813bbf065
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-07-31 01:28:08 +0300

    Update settings.py

--- Book_1/Alien invasion/settings.py
+++ Book_1/Alien invasion/settings.py
@@ -8,17 +8,25 @@ class Settings():
         #параметр пришельца
         self.fleet_drop_speed=10
         #ПАРАМЕТР СНАРЯДА
-        self.bullet_width=5
+        self.bullet_width=3000
         self.bullet_height=15
         self.bullet_color=(50,50,20)
         self.bullet_allowed=5
         #Темп ускорения игры
-        self.speed_scale=1.1
+        self.speed_scale=1.2
+        self.score_scale=1.5
 
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        self.ship_speed_factor=1.5
-        self.bullet_speed_factor=3.0
-        self.alien_speed_factor=1.0
+        self.ship_speed=1.5
+        self.bullet_speed=3.0
+        self.alien_speed=1.0
+        self.alien_score=50
         self.fleet_direction=1
+
+    def increase_speed(self):
+        self.ship_speed*=self.speed_scale
+        self.bullet_speed*=self.speed_scale
+        self.alien_speed*=self.speed_scale
+        self.alien_score*=self.score_scale
