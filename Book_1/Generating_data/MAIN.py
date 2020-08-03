77726d41a527a3b80f1f2ba6ca290a64b2e446a4
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-03 21:02:25 +0300

    Update MAIN.py

--- Book_1/Generating_data/MAIN.py
+++ Book_1/Generating_data/MAIN.py
@@ -1,1 +1,9 @@
+#Making simple figure
 
+import matplotlib.pyplot as plt
+
+squares=[1,4,9,16,25]
+fig, ax= plt.subplots() #fig - представляет весь рисунок или набор диаграмм. ax - представляет одну диаграмму на рисунке
+ax.plot(squares) # plot - строит что-то осмысленное из того, что мы ему дали
+
+plt.show() # show - открывает окно просмотра matplotlib и в этом окне выводит график
