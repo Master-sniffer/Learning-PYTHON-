4224367326fb0406385a2d25399dc27c639f8cfb
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-05 02:25:36 +0300

    Update Die_with_a_chance.py

--- Book_1/Generating Data/Die_with_a_chance.py
+++ Book_1/Generating Data/Die_with_a_chance.py
@@ -1,6 +1,8 @@
 #Ща мы будем мутить моделирование бросков кубиков (Предлагаю начать с 2 кубиков). Краткий экскурс: при броске кубика, мы имеем равные шансы получить число от 1 до 6, но когда мы кидаем 2 кубика, эти шансы меняется.
 #Plotly оч полезная вещь, если вы решили сделать визуализацию данных или прочих таких интересных вещей. Без лишних слов - ПОЕХАЛИ !
 
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
 from random import randint
 
 class Die():
@@ -17,4 +19,23 @@ for roll_num in range(1000):
   result = die.roll()
   results.append(result)
 
-...
+frequency=[]
+for value in range(1, die.num_sides+1):
+  frequencies=results.count(value)
+  frequency.append(frequencies)
+print (frequency)
+
+#Тут мы сделали это через словарь, но это необязательно
+#fre={}
+#for value in range(1, die.num_sides+1):
+#  frequencies=results.count(value)
+#  fre[value]=frequencies
+#print (fre)
+
+x_values=list(range(1, die.num_sides+1))
+data=[Bar(x=x_values, y=frequency)]
+
+x_axis_config={'title':"Result"}
+y_axis_config={"title":"Frequency of result"}
+my_layout= Layout(title="Results of rolling the dice 1000 times", xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data, 'layout': my_layout}, filename='d6.html')
