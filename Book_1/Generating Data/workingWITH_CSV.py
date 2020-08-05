bd5619f8bc16baa537a95cfadaf18680abc8a122
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-05 03:35:17 +0300

    Update workingWITH_CSV.py

--- Book_1/Generating Data/workingWITH_CSV.py
+++ Book_1/Generating Data/workingWITH_CSV.py
@@ -1,1 +1,17 @@
+#Ща мы будем работать с excel файлами и сможем чего-нибудь анализировать
 
+import csv 
+
+filename='sitka_weather_07-2018_simple.csv' # Файл находится в Extra FIles в generating data
+with open (filename) as f:
+  reader=csv.reader(f)
+  header_row=next(reader)
+  #for index, column_header in enumerate(header_row): #enemurate служит для того, чтобы возвращать индекс каждого элемента и его значение при переборке списка
+  #  print (index, column_header)
+
+  highs=[]
+  for row in reader:
+    high=int(row[5])
+    highs.append(high)
+
+print (highs)
