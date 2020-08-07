9a12b6439b2d6055f3be17ebcba56d0542d755c7
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-08 00:24:46 +0300

    Update working_with_hackernews.py

--- Book_1/Working with API/working_with_hackernews.py
+++ Book_1/Working with API/working_with_hackernews.py
@@ -14,5 +14,14 @@ for submission_id in submission_ids[:30]:
   response_dict=r.json()
 
   submission_dict={
-    d
+    'title': response_dict['title'],
+    'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
+    'comments' : response_dict['descendants'],
   }
+  submission_dicts.append(submission_dict)
+
+submission_dicts=sorted(submission_dicts, key=itemgetter('comments'), reverse=True)
+for submission_dict in submission_dicts:
+  print (f"Title: {submission_dict['title']}")
+  print (f"Discussion link: {submission_dict['hn_link']}")
+  print (f"comments: {submission_dict['comments']}")
