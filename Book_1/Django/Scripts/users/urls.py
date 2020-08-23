19a1552d7ef9145b08f4a30818e562c7b53d442e
Master_Sniffer <61323659+Master-sniffer@users.noreply.github.com>
2020-08-23 03:58:37 +0300

    Add files via upload

--- Book_1/Django/Scripts/users/urls.py
+++ Book_1/Django/Scripts/users/urls.py
@@ -1,9 +1,12 @@
 # определяем схемы URL для юзверей
 
 from django.urls import path, include
+from . import views
 
 app_name="users" # присвоили здесь users, чтобы django мог отличить эти URL адреса от других, которые принадлежат другим приложениям
 urlpatterns=[
     # включить URL авторизации по умолчанию
     path ('', include('django.contrib.auth.urls')),
+    # страница реги
+    path ('register/', views.register , name='register'),
 ]
--- Book_1/Django/Scripts/users/views.py
+++ Book_1/Django/Scripts/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    #регаем нового юзверя
+    if request.method != 'POST':
+        #вывод пустой формы реги
+        form=UserCreationForm()
+    else:
+        #Обработка заполненной формы
+        form=UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user=form.save() # идет сохранение данных юзера в БД
+            #Выполнение входа и перенаправка на домашнюю страницу
+            login (request, new_user)
+            return redirect ('learning_logs:index')
+        # вывод пустой или недействительной формы
+    context={'form': form}
+    return render (request, 'registration/register.html', context)
